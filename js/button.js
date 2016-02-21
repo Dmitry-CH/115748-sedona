5c6fcea2d6ec07588663daa52b53031fe00be5cd
Dmitry-CH <dmit.checkanov@yandex.ru>
2016-02-21 17:00:37 +0300

    Правки по критериям

--- js/button.js
+++ js/button.js
@@ -1,7 +1,36 @@
-var search = document.querySelector('.js-search'),
-    form   = document.querySelector('.search-form');
+var btnSearch      = document.querySelector('.js-search'),
+    formSearch     = document.querySelector('.search-form'),
+    btnSend        = document.querySelector('.btn-search-form'),
 
-search.addEventListener('click', function(event) {
+    form           = formSearch.querySelector('form'),
+    fieldArrival   = formSearch.querySelector('[name=arrival]'),
+    fieldDeparture = formSearch.querySelector('[name=departure]'),
+    fieldAdult     = formSearch.querySelector('[name=adult]');
+    
+
+btnSearch.addEventListener('click', function(event) {
+
   event.preventDefault();
-  form.classList.toggle('open');
+  formSearch.classList.toggle('open');
+  formSearch.classList.remove('error');
+  fieldArrival.focus();
+});
+
+form.addEventListener('submit', function(event) {
+
+  event.preventDefault();
+  if (!fieldArrival.value || !fieldDeparture.value || !fieldAdult.value) {
+    formSearch.classList.remove('error');
+    btnSearch.offsetWidth = btnSearch.offsetWidth;
+    formSearch.classList.add('error');
+    console.log('Нужно заполнить все поля!');
+  }else {
+    console.log('Отправка формы');
+  }
+});
+
+window.addEventListener('keydown', function(event) {
+  if (event.keyCode === 27 && formSearch.classList.contains('open')) {
+    formSearch.classList.remove('open');
+  };
 });
